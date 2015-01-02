68001ace7fd164a52d03109ba87e23eafc585746
Lewis Nyman <hello@lewisnyman.co.uk>
2015-01-02 12:38:40 +0000

    Person name is h3

--- templates/wunderhub_client_team.tpl.php
+++ templates/wunderhub_client_team.tpl.php
@@ -1,10 +1,10 @@
 <div class="team" data-ng-controller="WHTeamController">
   <div class="team__member person" data-ng-repeat="person in Team">
     <div class="person__picture" data-ng-bind-html="person.picture"></div>
-    <div class="person__name">
+    <h3 class="person__name">
       <span data-ng-bind="person.firstName"></span>
       <span data-ng-bind="person.lastName"></span>
-    </div>
+    </h3>
     <div class="person__job-title" data-ng-bind="person.jobTitle"></div>
     <div class="person__office" data-ng-bind="person.office"></div>
   </div>
